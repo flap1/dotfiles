58c31c878888f72cf53e16710f8bf068fe5392d3
flap1 <sho1seto@gmail.com>
2023-02-13 00:55:38 +0900

    update install_neovim

--- install/install_neovim.sh
+++ install/install_neovim.sh
@@ -8,3 +8,6 @@ check_and_install nvr "pip install neovim-remote"
 
 # git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
+# node
+[ -x "$(command -v npm)" ] && [ ! -e "$HOME/node_modules/remark" ] || cd "$HOME" || npm install remark remark-validate-links remark-inline-links remark-frontmatter
+
